54df6cbd0a50a093db8a9e6ba7478b914e07a560
Tsimafei <tima.kutsko@gmail.com>
2022-03-13 15:42:57 +0300

    Join with count

---
+++ MySql/Simple JOIN with COUNT.sql
@@ -0,0 +1,13 @@
+use cw_joinwithcount;
+
+insert into people (name) values ('Tima');
+insert into people (name) values ('Alina');
+
+insert into toys (name, people_id) values ('Zeliboba', 1);
+insert into toys (name, people_id) values ('PacaVaca', 1);
+insert into toys (name, people_id) values ('MacBook', 2);
+
+select people.id, people.name, count(toys.people_id) as toy_count from people
+	inner join toys on people.id = toys.people_id
+    group by people.id;
+    
