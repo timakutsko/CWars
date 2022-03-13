aa418954cafd7829dc72a1df66e6373ba743688e
Tsimafei <tima.kutsko@gmail.com>
2022-03-13 15:52:34 +0300

    some test

--- MySql/Simple JOIN with COUNT.sql
+++ MySql/Simple JOIN with COUNT.sql
@@ -10,4 +10,3 @@ insert into toys (name, people_id) values ('MacBook', 2);
 select people.id, people.name, count(toys.id) as toy_count from people
 	inner join toys on people.id = toys.people_id
     group by people.id;
-    
