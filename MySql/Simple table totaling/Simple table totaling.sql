c07d18b93317226334d827428456c0638068ac74
Tsimafei <tima.kutsko@gmail.com>
2022-03-13 17:54:41 +0300

    Simple table totaling

--- MySql/Simple table totaling/Simple table totaling.sql
+++ MySql/Simple table totaling/Simple table totaling.sql
@@ -4,20 +4,19 @@ select * from people;
 insert into people(name, points, clan) values('Tima', 75, 'Man');
 insert into people(name, points, clan) values('Alina', 85, 'Woman');
 
-DROP PROCEDURE IF EXISTS creator;
-DELIMITER //
-CREATE PROCEDURE creator (IN num INT, IN nameVar VARCHAR(45), IN clanVar VARCHAR(45))
-  BEGIN
-	  DECLARE i INT DEFAULT 0;
-	  DECLARE var TINYTEXT DEFAULT '';
-	  WHILE i < num DO
-		SET i = i + 1;
-		SET var = CONCAT(nameVar, i);
-		insert into people(name, points, clan) values(var, 30, clanVar);
-	  END WHILE;
-	END
-//
+CALL creator(3, 'Bro', 'Man');
+CALL creator(3, 'Sist', 'Woman');
 
-CALL creator(3, 'Bro', 'Man')//
-CALL creator(3, 'Sist', 'Woman')//
-DELIMITER ;
+insert into people(name, points) values('SomePerson1', 85);
+insert into people(name, points) values('SomePerson2', 135);
+insert into people(name, points, clan) values('Brooo', 50, 'Man');
+insert into people(name, points, clan) values('Sist', 50, 'Woman');
+
+select 
+	RANK() OVER (
+    ORDER BY sum(points) DESC) as current_rank,
+    IF(clan IS NULL, 'no clan specified', clan) as clan, 
+    sum(points) as total_points,
+    count(*) as total_people
+from people
+group by clan;
