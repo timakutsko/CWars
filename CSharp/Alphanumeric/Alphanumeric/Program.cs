5d2a313aa4341da474db7e84f18dd98a1f8241be
Tsimafei <tima.kutsko@gmail.com>
2022-04-24 22:47:12 +0300

    Alphanumeric

---
+++ CSharp/Alphanumeric/Alphanumeric/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Alphanumeric
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] checkArray = new string[]
+            {
+                "Mazinkaiser",
+                "hello world_",
+                "PassW0rd",
+                "         ",
+                "",
+            };
+
+            foreach(string str in checkArray)
+            {
+                Console.WriteLine(Kata.Alphanumeric(str).ToString());
+            }            
+        }
+    }
+
+    internal static class Kata
+    {
+        /// <summary>
+        /// Проверка, является ли строка ввода пользователя буквенно-цифровой. 
+        /// </summary>
+        public static bool Alphanumeric(string str)
+        {
+            bool result = true;
+            int numb = str.ToCharArray().Count(c => !Char.IsLetterOrDigit(c));
+            if (numb > 0 | str.Length == 0)
+            {
+                result = false;
+            }
+            return result;
+        }
+    }
+}
